bdf3c75e3d2ec8a6e0b012b6ae497530bce4ef3b
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-05-18 01:04:00 +0000

    [Finish]3-flash

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
+    list = List.new(list_params)
     list.save
     flash[:notice] = "投稿が成功しました"
     redirect_to list_path(list.id)
